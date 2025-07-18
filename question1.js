1c3882a07c2e45409f85933bca1f6e26e232adb2
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-18 12:00:37 +0530

    question1 is completed

---
+++ question1.js
@@ -0,0 +1,15 @@
+// 1.differnce between var,let and const 
+// ->var - function scope -pazhaya reethi,inside the function varibles are declaried,changeable
+// ->let - block scope-inside the block variables is declaried,not chnageable
+// ->const - block scope-inside the block variables is declaried,constant
+// var let and const is the function declarations of javascript
+
+
+var x = 10;
+console.log(x); 
+
+x = 20;          
+console.log(x); 
+
+var x = 30;      
+console.log(x); 
